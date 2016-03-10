6bbfaec92a2f704c016e21dcdb741c9ca03e2c0d
Mikhail Stolyarov <x3om6ak@gmail.com>
2016-03-10 15:59:46 +0300

    Второй коммит - ерунда

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Entry
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the BLOG index.")
+    latest_posts = Entry.objects.order_by('-cdate')[:5]
+    context = {'latest_posts': latest_posts}
+    return render(request, 'blog/index.html', context)
