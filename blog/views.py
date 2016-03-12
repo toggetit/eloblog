d035cc3cfb54ef1ad1d03aef6de392cd3ad479f4
Mikhail Stolyarov <x3om6ak@gmail.com>
2016-03-13 00:16:05 +0300

    Добавлен шаблон post.html и ссылка на него. Незначительные изменения index.html

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Entry
+from django.shortcuts import get_object_or_404
 
 def index(request):
     latest_posts = Entry.objects.order_by('-cdate')[:5]
     context = {'latest_posts': latest_posts}
     return render(request, 'blog/index.html', context)
+
+def post(request, entry_id):
+    post = get_object_or_404(Entry, pk=entry_id)
+    return render(request, 'blog/post.html', {'post': post})
