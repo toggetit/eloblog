a1deedce5fecfee03bb48aa757fcda3e6221a3de
Mikhail Stolyarov <x3om6ak@gmail.com>
2016-04-08 15:08:55 +0300

    В админку добавлены атрибуты постов

--- eloblog/admin.py
+++ eloblog/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Entry
 
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isDraft', 'cdate', 'edate')
+
+admin.site.register(Entry, EntryAdmin)
